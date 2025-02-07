65bec6cc59cc00801ced0b32c8eacc9edfb21ec0
celestinaodili <odili.tina@gmail.com>
2025-02-07 23:05:45 +0100

    Update NumberController.cs

--- NumberClassificationAPI/Controllers/NumberController.cs
+++ NumberClassificationAPI/Controllers/NumberController.cs
@@ -1,90 +1,67 @@
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Linq;
-using System.Net.Http;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 [ApiController]
-[Route("api")]
-public class ClassifyNumberController : ControllerBase
+[Route("api/numbers")]
+public class NumberController : ControllerBase
 {
-    [HttpGet("classify-number")]
-    public async Task<IActionResult> ClassifyNumber([FromQuery] string number)
+    [HttpGet("classify/{number?}")]
+    public IActionResult ClassifyNumber([FromRoute] int? number, [FromQuery] int? queryNumber)
     {
-        // Try parsing the number as a double to handle both integers and floating-point numbers
-        if (!double.TryParse(number, out double num))
+        int? finalNumber = number ?? queryNumber;
+
+        if (!finalNumber.HasValue)
         {
-            return BadRequest(new { number, error = true });
+            return BadRequest(new
+            {
+                type = "https://tools.ietf.org/html/rfc9110#section-15.5.1",
+                title = "One or more validation errors occurred.",
+                status = 400,
+                errors = new { number = new[] { "The number field is required." } }
+            });
         }
 
-        // Use the absolute value for classification and calculations
-        num = Math.Abs(num);
-
-        var properties = GetNumberProperties(num);
-        int digitSum = num.ToString().Sum(c => c - '0'); // Sum of absolute digit values
-
-        string funFact = await GetFunFact(num);
-
-        return Ok(new
+        var result = new
         {
-            number = num,
-            is_prime = IsPrime(num),
-            is_perfect = IsPerfect(num),
-            properties,
-            digit_sum = digitSum,
-            fun_fact = funFact
-        });
+            Number = finalNumber,
+            IsPrime = IsPrime(finalNumber.Value),
+            IsPerfect = IsPerfect(finalNumber.Value),
+            IsArmstrong = IsArmstrong(finalNumber.Value)
+        };
+
+        return Ok(result); // Returns HTTP 200 OK with JSON response
     }
 
-    private bool IsPrime(double n)
+    private bool IsPrime(int num)
     {
-        if (n < 2) return false;
-        for (int i = 2; i * i <= n; i++)
-            if (n % i == 0) return false;
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++)
+            if (num % i == 0) return false;
         return true;
     }
 
-    private bool IsPerfect(double n)
+    private bool IsPerfect(int num)
     {
-        if (n < 1) return false;
         int sum = 1;
-        for (int i = 2; i * i <= n; i++)
-            if (n % i == 0) sum += i + (i * i == n ? 0 : (int)(n / i));
-        return sum == n;
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                sum += i;
+                if (i != num / i) sum += num / i;
+            }
+        }
+        return sum == num && num != 1;
     }
 
-    private string[] GetNumberProperties(double n)
+    private bool IsArmstrong(int num)
     {
-        var properties = new System.Collections.Generic.List<string>();
-        if (IsArmstrong(n)) properties.Add("armstrong");
-        properties.Add(n % 2 == 0 ? "even" : "odd");
-        return properties.ToArray();
-    }
-
-    private bool IsArmstrong(double n)
-    {
-        int sum = 0, temp = (int)Math.Abs(n), digits = temp.ToString().Length;
+        int sum = 0, temp = num, digits = num.ToString().Length;
         while (temp > 0)
         {
-            int digit = temp % 10;
-            sum += (int)Math.Pow(digit, digits);
+            sum += (int)Math.Pow(temp % 10, digits);
             temp /= 10;
         }
-        return sum == Math.Abs(n);
-    }
-
-    private async Task<string> GetFunFact(double n)
-    {
-        using var httpClient = new HttpClient();
-        string apiUrl = $"http://numbersapi.com/{(int)n}/math"; // Use the integer part for the fun fact
-        try
-        {
-            return await httpClient.GetStringAsync(apiUrl);
-        }
-        catch
-        {
-            return "No fun fact available";
-        }
+        return sum == num;
     }
 }
+
